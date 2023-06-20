5d412e4b92b2145a74f6c82bc0b1e664d315967d
Mohamed <developperw@gmail.com>
2023-06-20 14:03:11 -0700

    Print Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - Print _putchar followed by a new line
+ *
+ * Return: Return 0
+ */
+int main(void)
+{
+        char alx[10] = "_putchar";
+
+        int i;
+
+        for (i = 0; i < 8; i++)
+        {
+                _putchar(alx[i]);
+        }
+
+        _putchar(10);
+
+        return (0);
+}
