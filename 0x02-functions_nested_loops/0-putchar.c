a26fe46caa15e9897a41d4fa54f69be9c713d2a7
Mohamed <developperw@gmail.com>
2023-06-20 08:05:55 -0700

    Print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,12 +7,15 @@
 int main(void)
 {
 	char alx[10] = "_putchar";
+
 	int i;
+
 	for (i = 0; i < 8; i++)
 	{
 		_putchar(alx[i]);
 	}
-	_putchar(10);
-	return (0);
 
+	_putchar(10);
+
+	return (0);
 }
