a288f73ad082172914fda617728f1014e105110b
Mohamed <developperw@gmail.com>
2023-06-29 14:41:35 -0700

    Add new line at end of file

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,34 +1,44 @@
 #include "main.h"
+
 /**
- * print_number - prints an integer.
- * @n: input integer.
- * Return: no return.
- */
-void print_number(int n)
+* rot13 -  encodes a string using rot13
+* @str:the string targeted
+*Return: returns the encoded string
+*/
+
+char *rot13(char *str)
 {
-	unsigned int m, d, count;
+	int index1, index2;
 
-	if (n < 0)
-	{
-		_putchar(45);
-		m = n * -1;
-	}
-	else
-	{
-		m = n;
-	}
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+											 'G', 'H', 'I', 'J', 'K', 'L',
+											 'M', 'N', 'O', 'P', 'Q', 'R',
+											 'S', 'T', 'U', 'V', 'W', 'X',
+											 'Y', 'Z', 'a', 'b', 'c', 'd',
+											 'e', 'f', 'g', 'h', 'i', 'j',
+											 'k', 'l', 'm', 'n', 'o', 'p',
+											 'q', 'r', 's', 't', 'u', 'v',
+											 'w', 'x', 'y', 'z'};
 
-	d = m;
-	count = 1;
-
-	while (d > 9)
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+											 'T', 'U', 'V', 'W', 'X', 'Y',
+											 'Z', 'A', 'B', 'C', 'D', 'E',
+											 'F', 'G', 'H', 'I', 'J', 'K',
+											 'L', 'M', 'n', 'o', 'p', 'q',
+											 'r', 's', 't', 'u', 'v', 'w',
+											 'x', 'y', 'z', 'a', 'b', 'c',
+											 'd', 'e', 'f', 'g', 'h', 'i',
+											 'j', 'k', 'l', 'm'};
+	while (str[++index1])
 	{
-		d /= 10;
-		count *= 10;
+		for (index2 = 0; index2 < 52; index2++)
+		{
+			if (str[index1] == alphabet[index2])
+			{
+				str[index1] = rot13key[index2];
+				break;
+			}
+		}
 	}
-
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((m / count) % 10) + 48);
-	}
+	return (str);
 }
