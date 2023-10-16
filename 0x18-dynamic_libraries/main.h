d00d5b7e436e23ac0f30b415b9e25207c5d37284
medrythm <developperw@gmail.com>
2023-10-16 20:15:10 +0100

    .

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef DYNLIB
-#define DYNLIB
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 #include <stdlib.h>
 
@@ -24,4 +24,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+#endif /* _MAIN_H_ */
